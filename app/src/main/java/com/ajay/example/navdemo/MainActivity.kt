e126e11830011a5f0a7abb8afd42daf3cb473c43
ajaypatel <patel.ajay745@gmail.com>
2022-12-20 23:01:45 -0500

    Initial commit

---
+++ app/src/main/java/com/ajay/example/navdemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ajay.example.navdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ajay.example.navdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/ajay/example/navdemo/SecondFragment.kt
@@ -0,0 +1,31 @@
+package com.ajay.example.navdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.ajay.example.navdemo.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding=DataBindingUtil.inflate(inflater,R.layout.fragment_second, container, false)
+
+        var input: String? =requireArguments().getString("user_input")
+        binding.textView.text=input.toString()
+
+        return binding.root
+    }
+
+
+}
